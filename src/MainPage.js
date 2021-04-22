97f925761309c26851057e384a9170fafaa5f62e
BRADLEYLIM <bradleylim92@gmail.com>
2021-04-22 16:14:25 -0700

    finished mainpage visuals

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
 import ClipLogo from './images/clip.png';
 import Hamburger from './images/hamburger.PNG';
+import samplePic from './images/samplepic.PNG';
+import collabs from './images/collabs.PNG';
 
 import './App.css';
 import Navbar from './Navbar';
@@ -28,7 +31,51 @@ class MainPage extends Component {
                         <h1><a href="#"><img src={ClipLogo}/></a></h1>
                     </div>
                 </div>
-                {/* <Navbar/> */}
+                <div class="containerEqual">
+                    <div>
+                        <img style={{border:"outset"}} src={samplePic}/>
+                    </div>
+                    <div class="presentIdeas">
+                        <h1 class="demo1">PRESENT YOUR IDEAS <br/> WITH CONFIDENCE</h1>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper suscipit luctus. Sed nec purus viverra, fermentum ex a, volutpat nunc. Cras luctus lobortis urna eget fringilla. Sed condimentum est eu porttitor imperdiet. Curabitur egestas, in accumsan purus molestie.
+                        <p></p>
+                        <button class="startNowButton" type="button">START NOW</button>
+                    </div>
+                </div>
+
+                <div class="whyUseClip">
+                    <h1 style={{textAlign:"center", paddingTop:"40px", fontSize:"42px"}}>WHY USE CLIP</h1>
+                    <div class="containerSpace">
+                        <div class="presentIdeas">
+                            <h1 class="demo1">SHARE YOUR IDEAS <br/> IN FULL QUALITY</h1>
+                            Say <i>'Goodbye'</i> to compressing your files
+                            <p></p>
+                            Clip allows you to upload your files up to 100mbs without compression. Never compromise quality over convience ever again. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper suscipit luctus. Sed nec purus viverra, fermentum ex a, volutpat nun
+                        </div>
+                        <div class="graySpace"></div>
+                    </div>
+                    <div class="containerSpace">
+                        <div class="graySpace"></div>
+                        <div class="presentIdeas">
+                            <h1 class="demo1">BE CLEAR AND CONCISE</h1>
+                            <i>Simplify your presentation</i>
+                            <p></p>
+                            Clip allows you to present your ideas on a plane background without other third-party interfaces. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper ullamcorper ullamcorper suscipit luctus.
+                        </div>
+                    </div>
+                    <div class="containerSpace">
+                        <div class="presentIdeas">
+                            <h1 class="demo1">CONVIENCE AND <br/> CONTROL</h1>
+                            <i>Control how YOU share YOUR files.</i>
+                            <p></p>
+                            Clip allows you to share your files with small URLS. Allow your files to be seen publicly or password protected to secure files. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper suscipit luctus.
+                        </div>
+                        <div class="graySpace"></div>
+                    </div>
+                </div>
+                <h1 style={{textAlign:"center", marginTop:"40px"}} class="demo1">OUR RECENT COLLABORATIONS</h1>
+                <img style={{position:"relative", left:"20%", marginTop: "50px", marginBottom: "50px"}} src={collabs}/>
+                
             <Switch>
                 <Route path="/nav">
                     <Navbar />
